24df8bddcf7970f50138eaed7c9fbcfb9b22b0bb
neuroubiquitous <silkmachina@pm.me>
2022-05-25 07:22:03 -0700

    update on launcher. Need to work on connecting

--- SQL Scripts/create-db-cxc.sql
+++ SQL Scripts/create-db-cxc.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS 'cxc';
-CREATE DATABASE 'cxc';
+DROP SCHEMA IF EXISTS 'cxc';
+CREATE SCHEMA 'cxc';
 USE 'cxc';
 
 CREATE TABLE 'employees' (
@@ -11,7 +11,7 @@ CREATE TABLE 'employees' (
 'reports_to' int(11) DEFAULT NULL,);
 
 INSERT INTO 'employees' VALUES (1000,‘Addison', 'Bond', ‘Relations Director’, 65831,NULL,10);
-INSERT INTO 'employees' VALUES (2273,‘Dennis','Thompson’,‘Financial Administrator, 65822,1000,1);
+INSERT INTO 'employees' VALUES (2273,‘Dennis','Thompson’,‘Financial Administrator', 65822,1000,1);
 INSERT INTO 'employees' VALUES (4274,‘Jessica', 'Snow', ‘Case Manager’, 121009,1000,2);
 INSERT INTO 'employees' VALUES (4680,‘Dennis', 'Kim’, ‘Patient Advocate’, 89654,1000,2);
 INSERT INTO 'employees' VALUES (1851,‘Todd', 'Gallagher’, ‘Service Coordinator’, 102010,1000,1);
--- src/main/java/com/revature/Launcher.java
+++ src/main/java/com/revature/Launcher.java
@@ -1,7 +1,34 @@
 package com.revature;
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.revature.controllers.AuthenticationController;
+import com.revature.controllers.EmployeeController;
+import com.revature.utilities.ConnectionFactory;
+import io.javalin.Javalin;
 
 public class Launcher {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EmployeeController ec = new EmployeeController();
+        AuthenticationController ac = new AuthenticationController();
+        //Testing Database Connectivity - just testing whether our Connection
+        try(Connection conn = ConnectionFactory.getConnection()){
+            System.out.println("Connection Successful yay!");
+
+        } catch(SQLException e) {
+                System.out.println("Connection failed");
+                e.printStackTrace();
+        }
+
+        //Javalin starts (which creates the connection, done
+        Javalin app = Javalin.create(
+                config -> {
+                        config.enableCorsForAllOrigins(); //
+                }
+        ).start(3000);
+        //Now we need our endpoints
+        app.get("/employee", ecgetEmployeesHandler);
+        app.post("/employee", ec.insertEmployeesHandler);
+        //In the future, we will also add a log in function
+        app.post("/login", ac.loginHandler);
     }
 }
