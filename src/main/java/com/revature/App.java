e54f64f6ca820428ee3a0da88bde79d3a02b0548
neuroubiquitous <silkmachina@pm.me>
2022-05-26 12:17:40 -0700

    I was able to isolate Javalin and have it running by itself. The maven on my p1demo was not work. This one works because I was not using local maven. App.java tests Javalin. Ignore the other two java files.

---
+++ src/main/java/com/revature/App.java
@@ -0,0 +1,11 @@
+package com.revature;
+
+
+import io.javalin.Javalin;
+
+public class App {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7070);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
